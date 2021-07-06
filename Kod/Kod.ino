a0af7fd1d18bce252f6e14b21aa747d989fd620f
Matox Svk <75246510+MatoXsvk@users.noreply.github.com>
2021-07-06 21:46:20 +0200

    cODE EDIT

--- Kod/Kod.ino
+++ Kod/Kod.ino
@@ -1,13 +1,15 @@
 #include <SPI.h>
 #include <MFRC522.h>
- 
+
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
+
+
 // Pre jednoduchšie pridavanie vacerých čipov dajte pred každý riadok čo má na konci //*|*// znak // a pre zrušenie ho zas
 
-// 1. Získajte kópiu tohto kodu (github kopia - https://github.com/MatoXsvk/Arduino_rfid), dalej uids, ktore chcete aby boli povolene (aj staré - spravte krok 2, potom po privedeni 5V na pin A0 vám to na okno vypíše staré uid kódy, ktoré môžete použiť)
+// 1. Získajte kópiu tohto kodu (github kopia - https://github.com/MatoXsvk/Arduino_rfid), dalej 'uid' kody, ktore chcete aby boli povolene (aj staré - spravte krok 2, potom po privedeni 5V na pin A0 vám to na okno vypíše staré uid kódy, ktoré môžete použiť)
 // 2. V ľavom hornom rohu otvorte lupu
 // 3. Priložte čip ktorý chcete pridať
 // 5. Skopírujte informáciu za vetou 'UID tag:' od prvého " do " do riadku kde ukazuje šípka pred } a dajte čiarku a medzeru :  String uids[] = {"B7 35 2E D6", ... , "novy uid", };
@@ -22,6 +24,7 @@ String uids[] = {};
 
 
 
+
 int rele = 2;
 int bzuciak = 3;
 int ledSpravne = 4;
@@ -29,7 +32,7 @@ int ledZle = 5;
 
 int codesBTN = A0;
 
-void setup() 
+void setup()
 {
   pinMode(rele, OUTPUT);
   pinMode(ledSpravne, OUTPUT);
@@ -37,7 +40,7 @@ void setup()
   pinMode(bzuciak, OUTPUT);
 
   pinMode(codesBTN, INPUT);
-  
+
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
@@ -45,126 +48,126 @@ void setup()
   Serial.println();
 
 }
-void loop() 
+void loop()
 {
   if (digitalRead(codesBTN) == HIGH) printUIDS();
-  
+
   // Look for new cards and select one of the cards
-  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) 
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial())
   {
     return;
   }
-  
+
   //Show UID on serial monitor
   Serial.print("UID tag : "); //*|*//
   String content = "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  for (byte i = 0; i < mfrc522.uid.size; i++)
   {
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  
+
   content.toUpperCase();
-  
+
   Serial.print("\"");
   Serial.print(content.substring(1));
   Serial.print("\", ");
-  
+
   Serial.println(); //*|*//
   Serial.print("Message : "); //*|*//
-  
-  if (isInAuthorized(content)) 
+
+  if (isInAuthorized(content))
   {
     Serial.println("Authorized access"); //*|*//
     Serial.println(); //*|*//
-    
+
     signalizacia(true);
     otvoritStroj();
-  } 
-  else 
+  }
+  else
   {
     Serial.println(" Access denied"); //*|*//
     Serial.println(); //*|*//
-    
+
     digitalWrite(rele, LOW);
-    signalizacia(false);    
+    signalizacia(false);
   }
   delay(3000);
-} 
+}
 
-bool isInAuthorized(String value) 
+bool isInAuthorized(String value)
 {
-  for (int uidIndex = 0; uidIndex < sizeof(uids)/sizeof(uids[0]); uidIndex++) 
+  for (int uidIndex = 0; uidIndex < sizeof(uids) / sizeof(uids[0]); uidIndex++)
   {
-    if (value.substring(1) == uids[uidIndex]) return true; // Make sure you change this with your own UID number 
+    if (value.substring(1) == uids[uidIndex]) return true; // Make sure you change this with your own UID number
   }
   return false;
 }
 
-void signalizacia(bool overenie) 
+void signalizacia(bool overenie)
 {
-  if (overenie) 
+  if (overenie)
   {
     digitalWrite(ledSpravne, HIGH);
-    
-    for (int i = 3; i >= 1; i--) 
+
+    for (int i = 3; i >= 1; i--)
     {
       int timeDuration = 25;
       if (i == 1)timeDuration = 45;
       for (int j = 0; j < timeDuration; j++) {
         digitalWrite(bzuciak, HIGH);
         delay(i);
-      
+
         digitalWrite(bzuciak, LOW);
         delay(1);
       }
       delay(25);
     }
-    
+
     digitalWrite(ledSpravne, LOW);
     return;
   }
 
   digitalWrite(ledZle, HIGH);
-  for (int i = 0; i < 50; i++) 
+  for (int i = 0; i < 50; i++)
   {
     digitalWrite(bzuciak, HIGH);
     delay(1);
-    
+
     digitalWrite(bzuciak, LOW);
     delay(1);
   }
   delay(50);
-  for (int i = 0; i < 100; i++) 
+  for (int i = 0; i < 100; i++)
   {
     digitalWrite(bzuciak, HIGH);
     delay(3);
-    
+
     digitalWrite(bzuciak, LOW);
     delay(1);
   }
   digitalWrite(ledZle, LOW);
-  
+
 }
 
-void otvoritStroj() 
+void otvoritStroj()
 {
   digitalWrite(rele, HIGH);
   delay(500);
   digitalWrite(rele, LOW);
 }
 
-void printUIDS() 
+void printUIDS()
 {
   Serial.println("Stare uid kody: ");
-  
-  for (int uidIndex = 0; uidIndex < sizeof(uids)/sizeof(uids[0]); uidIndex++)
+
+  for (int uidIndex = 0; uidIndex < sizeof(uids) / sizeof(uids[0]); uidIndex++)
   {
     Serial.print("\"");
     Serial.print(uids[uidIndex]);
     Serial.print("\", ");
   }
-  Serial.println();
+  Serial.println("\n");
   delay(2000);
 }
